c42dd1d54f5be29f3f3ee3e87962e01f0cef1542
pixel-perfect-metodology <35000284+pixel-perfect-metodology@users.noreply.github.com>
2023-05-04 18:13:30 +0700

    Create main.py

---
+++ .codesandbox/main.py
@@ -0,0 +1,21 @@
+import importlib
+import sys
+print('sys.version', sys.version)
+print('')
+
+# from day_1 import *
+# or
+# import day_2
+# or
+# from lessons import day_17
+
+last_day = 1
+
+for lesson_day in range(100):
+    if lesson_day == last_day:
+        try:
+            module_name = 'lessons.day_' + str(lesson_day)
+            print('trying to import', module_name, 'module')
+            importlib.import_module(module_name)
+        except ModuleNotFoundError as e:
+            print(repr(e))
