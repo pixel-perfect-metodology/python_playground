7302c4dfcdc17eb5df4b7bc2a7a998f1869d6d97
pixel-perfect-metodology <35000284+pixel-perfect-metodology@users.noreply.github.com>
2023-05-04 18:22:14 +0700

    Create day_14.py

---
+++ lessons/day_14.py
@@ -0,0 +1,95 @@
+print('\n\nday 14\n\n')
+
+print('# Polymorphism')
+
+x = "Hello World!"
+print(len(x))
+
+print('')
+mytuple = ("apple", "banana", "cherry")
+print(len(mytuple))
+
+print('')
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(len(thisdict))
+
+print('')
+
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Drive!")
+
+
+class Boat:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail!")
+
+
+class Plane:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Fly!")
+
+
+car1 = Car("Ford", "Mustang")  # Create a Car class
+boat1 = Boat("Ibiza", "Touring 20")  # Create a Boat class
+plane1 = Plane("Boeing", "747")  # Create a Plane class
+
+for x in (car1, boat1, plane1):
+    x.move()
+
+
+print('')
+
+
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Move!")
+
+
+class Car(Vehicle):
+    pass
+
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sail!")
+
+
+class Plane(Vehicle):
+    def move(self):
+        print("Fly!")
+
+
+car1 = Car("Ford", "Mustang")  # Create a Car object
+boat1 = Boat("Ibiza", "Touring 20")  # Create a Boat object
+plane1 = Plane("Boeing", "747")  # Create a Plane object
+
+for x in (car1, boat1, plane1):
+    print('')
+    print(x.brand)
+    print(x.model)
+    x.move()
+
+
+print('')
