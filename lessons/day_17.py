82f92c75a9cc0be48045e25440802679b05a49d3
pixel-perfect-metodology <35000284+pixel-perfect-metodology@users.noreply.github.com>
2023-05-04 18:23:16 +0700

    Create day_17.py

---
+++ lessons/day_17.py
@@ -0,0 +1,18 @@
+print('\n\nday 17\n\n')
+
+print('# Datetime')
+
+print('')
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+print(x.year)
+print(x.strftime("%A"))
+
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+
+x = datetime.datetime(2018, 6, 1)
+print(x.strftime("%B"))
